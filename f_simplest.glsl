5869dc0a89192c4b46e0588958d9f7143f878453
DeporS <depores1337@gmail.com>
2023-06-14 20:23:35 +0200

    dodanie drugiego zrodla swiatla

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -9,6 +9,7 @@ out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej 
 in vec4 ic; 
 in vec4 n;
 in vec4 l;
+in vec4 l2;
 in vec4 v;
 in vec2 iTexCoord0;
 
@@ -44,12 +45,14 @@ void main(void) {
 
 	//Znormalizowane interpolowane wektory
 	vec4 ml = normalize(l);
+	vec4 ml2 = normalize(l2);
 	vec4 mv = normalize(v);
 	vec2 tc = parallax(mv, iTexCoord0, 700, 0.03);
 	vec4 mn = normalize(vec4(texture(textureMap1, tc).rgb*2-1,0));
 	
 	//Wektor odbity
 	vec4 mr = reflect(-ml, mn);
+	vec4 mr2 = reflect(-ml2, mn);
 
 	//Parametry powierzchni
 	vec4 kd = texture(textureMap0, tc); 
@@ -59,5 +62,10 @@ void main(void) {
 	//Obliczenie modelu oświetlenia
 	float nl = clamp(dot(mn, ml), 0, 1);
 	float rv = pow(clamp(dot(mr, mv), 0, 1),25);
-	pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+	pixelColor = vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+
+	//Obliczenie modelu oświetlenia
+	float nl2 = clamp(dot(mn, ml2), 0, 1);
+	float rv2 = pow(clamp(dot(mr2, mv), 0, 1),25);
+	pixelColor += vec4(kd.rgb * nl2, kd.a) + vec4(ks.rgb*rv2, 0);
 }
--- v_simplest.glsl
+++ v_simplest.glsl
@@ -17,6 +17,7 @@ in vec4 c3;
 //Zmienne interpolowane
 out vec4 ic;
 out vec4 l;
+out vec4 l2;
 out vec4 n;
 out vec4 v;
 out vec2 iTexCoord0;
@@ -25,10 +26,12 @@ out vec2 iTexCoord0;
 
 void main(void) {
     mat4 invTBN = mat4(c1,c2,c3,vec4(0,0,0,1));
-    vec4 lp = vec4(0, 15, -6, 1); //przestrzeń świata
+    vec4 lp = vec4(15, 25, 15, 1); //przestrzeń świata
+    vec4 lp2 = vec4(-15, 25, -15, 1); //przestrzeń świata
     //l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
     //v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
     l = normalize(invTBN*(inverse(M)*lp-vertex));
+    l2 = normalize(invTBN*(inverse(M)*lp2-vertex));
     v = normalize(invTBN*(inverse(V*M)*vec4(0,0,0,1)-vertex));
     n = vec4(0,0,1,0);
     //n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
