5f1206c77b4378ce1a462ac843e8f75959aba4aa
DeporS <depores1337@gmail.com>
2023-06-22 22:03:25 +0200

    tak

---
+++ Cube.cpp
@@ -0,0 +1,19 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include "constants.h"
+#include "lodepng.h"
+#include "shaderprogram.h"
+#include "myCube.h"
+#include "myTeapot.h"
+#include <iostream>
+#include <vector>
+
+class Cube {
+public:
+	glm::mat4 M_new = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, z)); // pozycja
+};
---
+++ floor.cpp
@@ -0,0 +1,7 @@
+#include "myCube.h"
+
+class MyFloor
+{
+	public:
+		
+}
